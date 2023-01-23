0d4ebdfab66e7ed59fd65b778493c9e5c5891594
Sovima <malashchenko1329@gmail.com>
2023-01-22 23:39:52 -0500

    README updates

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # Reward points
 
-## Project description
+##### Table of Contents  
+[Walkthrough](##Walkthrough)  
+[Project description](##Description)
+[Usage](##Usage)
+[Adding a new rule](##Adding)
+
+
+
+## Walkthrough
+
+For the video walk through the functionalities of the website, go to [video walkthrough](https://youtu.be/S1-GgV5g3O8)
+
+
+## Description
 
 This module implements a web server that runs on localhost, collects transactions, and allows to track reward points for all transactoins as well as individually. 
 
@@ -18,28 +31,30 @@ Note that rule 5 and rule3 are not present as more points can be earned by fully
 
 The rules package is used in `transactions/transactions.go` which implements the pipeline for collecting transactions, collecting rules, and applying the rules to collect reward points.
 
-Lastly, `SofyaMalashchenko.go`
+Lastly, `SofyaMalashchenko.go` contains implementation of the server that runs on local host and combines above mentioned packages and files and gives an easy-to-use web application. 
 
+For further information on the specific files and functions, please view the comments in `rules.go`, `transactions.go`, and `SofyaMalashchenko.go`.
 
-## Walkthrough
-
-For the video walk through the functionalities of the website, go to [video walkthrough](https://youtu.be/S1-GgV5g3O8)
-
+## Usage
 ## Adding a new rule
 
-To add a new rule, create a new rule struct that has GetRequirements and ApplyRule methods. For example, see rule 7:
+Current implementation of the rule structs allows for an easy addition of new rules. 
+
+To add a new rule, create a new rule struct that has GetRequirements and ApplyRule methods as well as update `collectRewards( transactions map[string]int ) []Rule` function in rules.go to add the new rule. These implementations should be added in the `rules.go` file.
+
+
+For example, see rule 8:
 ```
-var REQUIREMENT7 = map[string]int {"tim_hortons": 0, "subway": 0, "sportcheck" : 0,}
+var REQUIREMENT8 = map[string]int {"tim_hortons": 1000, "subway": 1000, "sportcheck" : 6500,}
 
-type Rule7 struct {}
+// An additional rule that is a combination of rule6 * 2 and rule4
+type Rule8 struct {}
 
-
-func (r *Rule7) GetRequirements() map[string] int {
-	return REQUIREMENT7
+func (r *Rule8) GetRequirements() map[string] int {
+	return REQUIREMENT8
 }
 
-
-func (r *Rule7) ApplyRule() int {
-	return 1
+func (r *Rule8) ApplyRule() int {
+	return 300
 }
 ```
--- transactions/rules/rules.go
+++ transactions/rules/rules.go
@@ -1,10 +1,16 @@
+// This is the package that contains all of the 
+// logic for collecting rules and 
 package rules
 
 import (
 	"math"
 )
 
-// The values are given in cents
+// The following variables store 
+// minimum requirements for the corresponding rule
+// For example REQUIREMENT1 stores requirements for rule1
+// That is, $25 tims, $25 subway, and $75 sportcheck
+// Note that all amounts are stored in cents
 var REQUIREMENT1 = map[string]int {"tim_hortons": 2500, "subway": 2500, "sportcheck" : 7500,}
 var REQUIREMENT2 = map[string]int {"tim_hortons": 2500, "subway": 0, "sportcheck" : 7500,}
 var REQUIREMENT3 = map[string]int {"tim_hortons": 0, "subway": 0, "sportcheck" : 7500,}
@@ -12,65 +18,113 @@ var REQUIREMENT4 = map[string]int {"tim_hortons": 1000, "subway": 1000, "sportch
 var REQUIREMENT5 = map[string]int {"tim_hortons": 1000, "subway": 0, "sportcheck" : 2500,}
 var REQUIREMENT6 = map[string]int {"tim_hortons": 0, "subway": 0, "sportcheck" : 2000,}
 var REQUIREMENT7 = map[string]int {"tim_hortons": 0, "subway": 0, "sportcheck" : 0,}
+var REQUIREMENT8 = map[string]int {"tim_hortons": 1000, "subway": 1000, "sportcheck" : 6500,}
 
 
 func CollectRewards( transactions map[string]int ) []Rule {
+	// this function implements the logic for collecting the
+	// maximum number of reward points for the input transactions
+	// input: 
+	//     transactions map[string]int : the total amounts spent for each merchant
+	// return:
+	//     []Rule : the list of rules that will be applied to the 
+	//              provided transactions
+
+	// Define the output variable
 	var output []Rule
 
-	// fmt.Println("Starting values")
-
-	// fmt.Println(transactions)
-
-	// Check how many rules1 can be applied
-
+	// Check how many rule1s can be applied
 	rule1 := new(Rule1)
+	// Append the right number of rule1 to the output
+	// and adjust the remaining amounts spent for each merchant
 	transactions, output = countRules(rule1, 1, transactions, output)
 	
 	
 	// Check how many 2 * rules4 can be applied
 	rule4 := new(Rule4)
+	// Append the right number of 2 * rule4 to the output
+	// and adjust the remaining amounts spent for each merchant
 	transactions, output = countRules(rule4, 2, transactions, output)
+
 
 	// Check how many rules2 can be applied
 	rule2 := new(Rule2)
+	// Append the right number of rule2 to the output
+	// and adjust the remaining amounts spent for each merchant
 	transactions, output = countRules(rule2, 1, transactions, output)
 
-	// Check how many rules4 can be applied
-	transactions, output = countRules(rule4, 1, transactions, output)
+
+	// Check how many rules8 can be applied
+	rule8 := new(Rule8)
+	// Append the right number of rule8 to the output
+	// and adjust the remaining amounts spent for each merchant
+	transactions, output = countRules(rule8, 1, transactions, output)
+
 
 	// Check how many 3*rules6 can be applied
 	rule6 := new(Rule6)
+	// Append the right number of rule6 to the output
+	// and adjust the remaining amounts spent for each merchant
 	transactions, output = countRules(rule6, 3, transactions, output)
+	
 
-	// Check how many rules3 can be applied
-	rule3 := new(Rule3)
-	transactions, output = countRules(rule3, 1, transactions, output)
+	// Check how many rules4 can be applied
+	// Append the right number of rule4 to the output
+	// and adjust the remaining amounts spent for each merchant
+
+	transactions, output = countRules(rule4, 1, transactions, output)
+
 
 	// Check how many rules6 can be applied
+
 	transactions, output = countRules(rule6, 1, transactions, output)
 
 
 	// Check how many rules7 can be applied
 	rule7 := new(Rule7)
+
+	// Collect the total remaining values between all merchants
+	// and add rule 7 to every remaining dollar
 	total_remaining_points := transactions["tim_hortons"] / 100 + transactions["subway"] / 100 + transactions["sportcheck"] / 100
 
+	// Add rule 7 to every remaining dollar
 	for i := 0; i < total_remaining_points; i++ {
 		output = append(output, rule7)
 	}
 
-	// fmt.Println("Remaining values")
-
-	// fmt.Println(transactions)
 	return output
 }
 
 
 func countRules(ruleToAdd Rule, group int, transactions map[string]int, output []Rule) (map[string]int, []Rule) {
+	// This function calculates the maximum number of 
+	// times a given rule can be applied to the remaining transactions
+	// input: 
+	//     ruleToAdd Rule : the type of rule we wish to add 
+	//     group int : the number of times a rule is grouped
+	//                 For example, if set to 3 and ruleToAdd is rule6
+	//                 we will only apply rule6 in groups of 3
+	//     transactions map[string]int : the total amounts spent for each merchant
+	//     output []Rule : the list of so far collected rules
+	// return:
+	//     map[string]int : the updated remaining values spent on each merchant
+	//     []Rule : the updated list of rules to be applied to the list of transactions
+
+
+	// Get the requirements for the added rule
 	ruleRequirements := ruleToAdd.GetRequirements()
+
+	// In case any of the required values is 0 we want to aviod division by zero
 	max_with_zero := int(math.Max(float64(transactions["sportcheck"]), math.Max(float64(transactions["tim_hortons"]), float64(transactions["subway"]))))
+
+	// These values will store the maximum number of times a given rule can be 
+	// applied to the specific merchant
 	max_sportcheck := max_with_zero
 	max_tims := max_with_zero
 	max_subway := max_with_zero
+
+	// Now, if the restrictions are not 0, we can divide by them and update 
+	// maximum number of times a given rule can be applied to the specific merchant
 	if ruleRequirements["sportcheck"] > 0 {
 		max_sportcheck = transactions["sportcheck"] / ruleRequirements["sportcheck"] / group
 	}
@@ -81,8 +135,14 @@ func countRules(ruleToAdd Rule, group int, transactions map[string]int, output [
 		max_subway = transactions["subway"] / ruleRequirements["subway"] / group
 	}
 
+
+	// Note that the count for a given rule
+	// is bounded above by the smallest number of times that the given rule 
+	// can be applied to specific merchant. Hence we are taking the minimum
 	rule_count := int(math.Min(float64(max_sportcheck), math.Min(float64(max_tims), float64(max_subway))))
 
+	// Append the given rule to the output list rule_count of times
+	// and update the remaining transaction values for each merchant
 	for i := 0; i < rule_count; i++ {
 		for j := 0; j < group; j++ {
 			output = append(output, ruleToAdd)
@@ -92,19 +152,20 @@ func countRules(ruleToAdd Rule, group int, transactions map[string]int, output [
 		transactions["subway"] -= group * ruleRequirements["subway"]
 	}
 
-	// fmt.Println("Remaining transactions ", transactions)
-
-
 	return transactions, output
 }
 
-
-
+// This interface allows us to pass different rules in
+// the countRules function 
+// It lists the methods that each of the concrete classes
+// must implement
 type Rule interface {
 	GetRequirements() map[string] int
 	ApplyRule() int
 }
 
+// Below you will see individual rules that implement
+// the abstract Rule class
 
 // Rule1
 type Rule1 struct {}
@@ -125,7 +186,7 @@ type Rule2 struct {}
 
 
 func (r *Rule2) GetRequirements() map[string] int {
-	return REQUIREMENT1
+	return REQUIREMENT2
 }
 
 
@@ -189,17 +250,27 @@ func (r *Rule6) ApplyRule() int {
 	return 75
 }
 
-
-
 // Rule7
 type Rule7 struct {}
-
 
 func (r *Rule7) GetRequirements() map[string] int {
 	return REQUIREMENT7
 }
 
-
 func (r *Rule7) ApplyRule() int {
 	return 1
 }
+
+
+// Rule8
+// An additional rule that is a combination of rule6 * 2 and rule4
+type Rule8 struct {}
+
+func (r *Rule8) GetRequirements() map[string] int {
+	return REQUIREMENT8
+}
+
+func (r *Rule8) ApplyRule() int {
+	return 300
+}
+
